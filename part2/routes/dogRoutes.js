1e269f9c84706139f69284efec3c52ba9455550f
Raka <kaiwest@bigpond.com>
2025-06-20 12:42:04 +0000

    [automatic commit] 2025-06-20 12:42:04 UTC 1.147.76.223

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-
+/* GET /api/dogs */
+router.get('/dogs', async (req, res) => {
+    try {
+        const [rows] = await db.execute(`
+            SELECT d.name as dog_name, d.size, u.username as owner_username
+            FROM Dogs d
+            JOIN Users u on d.owner_id = u.user_id
+            `);
+        res.json(rows);
+    }catch(err){
+        res.status(500).json({ error: 'Failed to fetch dogs' });
+    }
+});
 
 module.exports = router;
