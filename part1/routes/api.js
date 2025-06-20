4a2684c835ed8a1cd32d41918cb6678481412380
Raka <kaiwest@bigpond.com>
2025-06-20 03:27:12 +0000

    [automatic commit] 2025-06-20 03:27:12 UTC 49.178.106.87

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -42,7 +42,7 @@ module.exports = function(db) {
                 FROM Users u
                 LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id
                 LEFT JOIN WalkRequests wrq ON wa.request_id = wrq.request_id
-                LEFT JOIN WalkRatings wrt ON 
+                LEFT JOIN WalkRatings wrt ON u.user_id = wrt.walker_id
                 `);
             res.json(rows);
         }catch(err){
