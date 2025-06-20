c4d349f830e73284ed361c827b04400deaffb92e
Raka <kaiwest@bigpond.com>
2025-06-20 11:31:21 +0000

    [automatic commit] 2025-06-20 11:31:21 UTC 1.147.76.223

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -81,7 +81,7 @@ router.get('/dogs', async (req, res) => {
       FROM Dogs
       WHERE owner_id = ?
       `, [owner_id]);
-    res.
+    res.json(rows);
   }catch(error){
     res.status(500).json({ error: 'Get dogs failed' });
   }
