f9a394743e1ff8451b804da33d94187fb1280802
Raka <kaiwest@bigpond.com>
2025-06-20 03:25:11 +0000

    [automatic commit] 2025-06-20 03:25:11 UTC 49.178.106.87

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -41,7 +41,7 @@ module.exports = function(db) {
                 SELECT u.username AS walker_username, .....
                 FROM Users u
                 LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id
-                LEFT JOIN WalkRequests wr ON 
+                LEFT JOIN WalkRequests wr ON wa.request_id = wr.request_id
                 `);
             res.json(rows);
         }catch(err){
