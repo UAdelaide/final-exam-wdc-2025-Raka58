02877e3a5d227c958fe52def004e8fccd30ce10a
Raka <kaiwest@bigpond.com>
2025-06-20 11:28:00 +0000

    [automatic commit] 2025-06-20 11:28:00 UTC 1.147.76.223

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -74,6 +74,7 @@ router.get('/dogs', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
+  const owner_id = req.session.user.user_id;
   
 });
 
