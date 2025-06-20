9162f7f0856feaf6cf3c6d9167d231aa750dd756
Raka <kaiwest@bigpond.com>
2025-06-20 10:00:52 +0000

    [automatic commit] 2025-06-20 10:00:52 UTC 1.147.76.223

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -65,7 +65,7 @@ router.post('/logout', async (req, res) => {
     if (err){
       return res.status(500).json({ error: 'Logout failed' });
     }
-    res.json({ message: ''})
+    res.json({ message: 'Logout successful' });
   });
 });
 
