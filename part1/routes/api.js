fd73596569793ec28b624538074bf5e3f679ea11
Raka <kaiwest@bigpond.com>
2025-06-20 03:11:21 +0000

    [automatic commit] 2025-06-20 03:11:21 UTC 49.178.106.87

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -6,7 +6,8 @@ module.exports = function(db) {
     /* GET /api/dogs */
     router.get('/dogs', async (req, res) => {
         try {
-
+            const [dogs] = await db.execute('SELECT * FROM books');
+            res.json(books);
         }catch(err){
             res.status(500).json({ error: 'Failed to fetch dogs' });
         }
