c0d4cb711aa1540b4e6b0899702a700f17e0811e
Raka <kaiwest@bigpond.com>
2025-06-20 02:28:50 +0000

    [automatic commit] 2025-06-20 02:28:50 UTC 49.178.106.87

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -3,11 +3,10 @@ var express = require('express');
 module.exports = function(db) {
     var router = express.Router();
 
-    
-}
+    return router;
+};
+
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
-
-module.exports = router;
