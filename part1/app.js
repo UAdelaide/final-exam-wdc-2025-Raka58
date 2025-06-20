e3470c87930fe832fae9e8e17c998d213a071f43
Raka <kaiwest@bigpond.com>
2025-06-20 02:15:21 +0000

    [automatic commit] 2025-06-20 02:15:21 UTC 49.178.106.87

--- part1/app.js
+++ part1/app.js
@@ -102,6 +102,19 @@ let db;
     `);
 
     // Insert data if Users table is empty
+    const [usersRows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+    if (usersRows[0].count === 0) {
+      await db.execute(`
+        INSERT INTO Users (username, email, password_hash, role)
+        VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('ben21', 'ben@example.com', 'hashed111', 'walker'),
+        ('emily22', 'emily@example.com', 'hashed222', 'owner')
+      `);
+    }
+    // Insert data if Dogs table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
