8f381fd20f066d516ad644c511f55fc16e907dfa
Raka <kaiwest@bigpond.com>
2025-06-20 03:40:41 +0000

    [automatic commit] 2025-06-20 03:40:41 UTC 49.178.106.87

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -25,7 +25,7 @@ module.exports = function(db) {
                     wr.duration_minutes, wr.location, u.username AS owner_username
                 FROM WalkRequests wr
                 JOIN Dogs d ON wr.dog_id = d.dog_id
-                JOIN Users u ON wr.owner_id = u.user_id
+                JOIN Users u ON d.owner_id = u.user_id
                 WHERE wr.status = 'open'
                 `);
             res.json(rows);
