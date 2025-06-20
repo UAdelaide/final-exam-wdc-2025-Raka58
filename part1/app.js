240d89773d9785c6b44ef2e7475efb258ee0603c
Raka <kaiwest@bigpond.com>
2025-06-20 02:12:56 +0000

    [automatic commit] 2025-06-20 02:12:56 UTC 49.178.106.87

--- part1/app.js
+++ part1/app.js
@@ -58,15 +58,18 @@ let db;
         FOREIGN KEY (owner_id) REFERENCES Users(user_id)
       )
     `);
-    // Create Dogs table if it doesn't exist
+    // Create WalkRequests table if it doesn't exist
     await db.execute(`
-      CREATE TABLE IF NOT EXISTS Dogs (
-        dog_id INT AUTO_INCREMENT PRIMARY KEY,
-        owner_id INT NOT NULL,
-        name VARCHAR(50) NOT NULL,
-        size ENUM('small', 'medium', 'large') NOT NULL,
-        FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-      )
+      CREATE TABLE WalkRequests (
+        request_id INT AUTO_INCREMENT PRIMARY KEY,
+        dog_id INT NOT NULL,
+        requested_time DATETIME NOT NULL,
+        duration_minutes INT NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
+    )
     `);
 
     // Insert data if Users table is empty
