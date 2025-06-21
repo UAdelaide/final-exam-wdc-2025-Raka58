a125772576899748472ddf484cca760ff16f53e7
Raka <kaiwest@bigpond.com>
2025-06-21 02:48:12 +0000

    [automatic commit] 2025-06-21 02:48:12 UTC 49.178.106.87

--- part1/app.js
+++ part1/app.js
@@ -170,7 +170,8 @@ let db;
         (SELECT user_id FROM Users u
         JOIN Dogs d ON u.user_id = d.owner_id
         WHERE d.name = 'Fred'),
-        
+        5,
+        'Very friendly!'
       `);
     }
 
