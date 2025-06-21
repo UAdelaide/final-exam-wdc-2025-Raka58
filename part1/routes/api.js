f463a45b6d472c8368e5640f683b0c433402da4b
Raka <kaiwest@bigpond.com>
2025-06-21 07:02:02 +0000

    [automatic commit] 2025-06-21 07:02:02 UTC 1.147.76.223

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -21,7 +21,7 @@ module.exports = function(db) {
     router.get('/walkrequests/open', async (req, res) => {
         try {
             // for each open walkrequest, returns:
-            
+            // request_id, dog_name, requested_time, duration_minutes
             const [rows] = await db.execute(`
                 SELECT wr.request_id, d.name AS dog_name, wr.requested_time,
                     wr.duration_minutes, wr.location, u.username AS owner_username
