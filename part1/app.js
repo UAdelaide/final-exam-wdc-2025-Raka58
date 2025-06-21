cccd37c98728e2e032744fdda8db65df5324daad
Raka <kaiwest@bigpond.com>
2025-06-21 02:38:56 +0000

    [automatic commit] 2025-06-21 02:38:56 UTC 49.178.106.87

--- part1/app.js
+++ part1/app.js
@@ -148,7 +148,7 @@ let db;
         VALUES
         ((SELECT request_id FROM WalkRequests wr
         JOIN Dogs d ON wr.dog_id = d.dog_id
-        WHERE ),
+        WHERE d.name = 'Fred'),
         (SELECT user_id FROM Users),
         'accepted')
       `);
