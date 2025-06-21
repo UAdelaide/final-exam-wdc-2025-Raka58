e9bf84e7fa06cac34f04fce081d135c2c9b45b51
Raka <kaiwest@bigpond.com>
2025-06-21 07:04:08 +0000

    [automatic commit] 2025-06-21 07:04:08 UTC 1.147.76.223

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -45,7 +45,7 @@ module.exports = function(db) {
             // walker_username, total_ratings, average_rating and completed_walks
 
             // using LEFT JOIN so that walkers with no matching walkapplications, walkrequests or
-            // walkratings
+            // walkratings are not excluded from the returned data ()
             const [rows] = await db.execute(`
                 SELECT u.username AS walker_username, COUNT(wrt.rating_id) AS total_ratings,
                 AVG(wrt.rating) AS average_rating, COUNT(wrq.request_id) AS completed_walks
