ff6709e32cdcebdd047208e821bd3151204bed25
Raka <kaiwest@bigpond.com>
2025-06-21 02:46:28 +0000

    [automatic commit] 2025-06-21 02:46:28 UTC 49.178.106.87

--- part1/app.js
+++ part1/app.js
@@ -167,8 +167,7 @@ let db;
         LIMIT 1),
         (SELECT user_id FROM Users
         WHERE username = 'bobwalker'),
-        'accepted')
-
+        'accepted'),
         (SELECT user_id FROM Users u
         JOIN Dogs d ON u.user_id = d.owner_id
         WHERE d.name = 'Fred'),
